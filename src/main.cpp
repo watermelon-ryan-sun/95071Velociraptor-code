ee0a524b8857b3ff06cfa16e4ebabf42a6b825ad
watermelon-ryan-sun <136938860+watermelon-ryan-sun@users.noreply.github.com>
2024-11-25 16:13:53 -0800

    after testing

--- src/main.cpp
+++ src/main.cpp
@@ -94,7 +94,7 @@ void autonomous() {
 			break;
 }*/
 //skills();
-//specialOdomSkills();
+specialOdomSkills();
 }
 
 /**
--- src/odom.cpp
+++ src/odom.cpp
@@ -6,11 +6,11 @@
 volatile double XPos;
 volatile double YPos;
 double previous;
-double moved = ((RM_MOTOR.get_position() + LM_MOTOR.get_position())/2) * driveTicksPerInch;
+double moved = ((RM_MOTOR.get_position() + LM_MOTOR.get_position())/2) / driveTicksPerInch;
 void recordPosition(){//repeatdly call
 IMU.set_rotation(0);
 while(true){
-    moved = ((RM_MOTOR.get_position() + LM_MOTOR.get_position())/2) * driveTicksPerInch-moved;
+    moved = (((RM_MOTOR.get_position() + LM_MOTOR.get_position())/2) / driveTicksPerInch) - previous;
     double currenttheta = IMU.get_rotation();
     double temp = cos(currenttheta) * moved;
      if (temp == std::nan("")) {
@@ -18,7 +18,7 @@ while(true){
         pros::delay(5000);
         continue;
     }
-       XPos +=temp;
+       XPos +=(cos(currenttheta)*moved);
        YPos += (sin(currenttheta) * moved);
        previous = moved;
         pros::lcd::print(0,"threading1%f, %f",temp, XPos);
@@ -26,14 +26,14 @@ while(true){
 }
 }
 void movePosition(double targetX, double targetY, bool faceBack){
-    double targetTheta = atan((targetY-YPos)/(targetX-XPos));
+    double targetTheta = 180 * atan((targetY-YPos)/(targetX-XPos))/M_PI;
     if(faceBack == true){
-        turn(targetTheta-180, 0.25,0.2,0.1,1,1);
-        moveBack(sqrt(((targetX-XPos)*(targetX-XPos)) - ((targetY-YPos)*(targetY-YPos))),0.2,0.3,0.2);
+        turn(targetTheta-180, 2.5,0.2,0.1,1,1);
+        moveBack(sqrt(((targetX-XPos)*(targetX-XPos)) + ((targetY-YPos)*(targetY-YPos))),0.2,0.3,0.2);
     }
     else{
-        turn(targetTheta, 0.25,0.2,0.1,1,1);
-        move(sqrt(((targetX-XPos)*(targetX-XPos)) - ((targetY-YPos)*(targetY-YPos))),0.2,0.3,0.2);
+        turn(targetTheta, 2.5,0.2,0.1,1,1);
+        move(sqrt(((targetX-XPos)*(targetX-XPos)) + ((targetY-YPos)*(targetY-YPos))),0.2,0.3,0.2);
     }
 }
 void testThread(){
